a0a8e8620080b005276cc7cf5464f0121cb160eb
Mauricio Hernández <mhernandez@localhost.localdomain>
2017-04-06 20:05:59 -0500

    schema.db y conexion con la base

--- puestos/app/controllers/usuario_controller.rb
+++ puestos/app/controllers/usuario_controller.rb
@@ -1,5 +1,6 @@
 class UsuarioController < ApplicationController
   def iniciarSesion
+  	
   end
 
   def cerrarSesion
---
+++ puestos/db/migrate/001_create_database.rb
@@ -0,0 +1,10 @@
+class CreateDatabase < ActiveRecord::Migration
+  def self.up
+    # insert schema.rb here
+  end
+
+  def self.down
+    # drop all the tables if you really need
+    # to support migration back to version 0
+  end
+end
