138ca437d7d5ca1de49a43b26c05416f9d50c8c3
zaide <xhanzary9@gmail.com>
2017-04-26 17:26:44 -0500

    Se arreglaron algunos detalles. Proyecto revisado

--- puestos/config/routes.rb
+++ puestos/config/routes.rb
@@ -3,12 +3,15 @@ Rails.application.routes.draw do
 
   resources :comidas
   #resources :calificas
-  devise_for :users
+  devise_for :users do
+  	get "perfil"
+  end
   root 'home#index'
  # resources :puestos
   resources :puestos #do 
   	resources :calificas
   #end
+  resources :principal
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
